73401a2cc6795042dbca38bf806c086827a548e7
Chamberista <manuelroldanrodriguez2311@gmail.com>
2025-10-21 13:07:26 +0200

    Ahora, verifica tu edad

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
 nombre = str(input("Introduce su nombre: "))
-
+edad = int(input("Introduce su edad: "))
 print("HOLA" , nombre)
+
+if edad >= 18:
+    print(nombre + " es mayor de edad")
+else:
+    print(nombre + "  es menor de edad")
