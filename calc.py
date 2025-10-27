e92855f7e8a1d352ac3887ebbb04cb32fe3208d6
Chamberista <manuelroldanrodriguez2311@gmail.com>
2025-10-27 11:09:21 +0100

    Division de dos numeros

--- calc.py
+++ calc.py
@@ -2,6 +2,7 @@ saluda = "Hola "
 
 print(saluda)
 
+#<<<<<<< HEAD
 num = 4
 num2 = 5
 multiplicacion = num * num2
@@ -10,3 +11,20 @@ print(multiplicacion)
 cuadrado = num ** 2
 cuadrado2 = num2 ** 2
 print(f"El cuadrado del primer numero es: {cuadrado}, el cuadrado del segundo numero es: {cuadrado2}")
+#=======
+num1 = 3
+num2 = 8
+suma = num1 + num2
+print(f"El resultado de la suma es: {suma}")
+
+num3 = 25
+num4 = 20
+resta = num3 - num4
+print(f"El resultado de la resta es: {resta}")
+#>>>>>>> feature-ff
+# -------------------------------------
+
+numero1 = 45
+numero2 = 5
+division = numero1 / numero2
+print(f"El resultado de la division es: {division}")
