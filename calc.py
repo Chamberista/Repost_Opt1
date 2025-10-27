1a603a7c29e397528083dc1857d145f442ee4db5
Chamberista <manuelroldanrodriguez2311@gmail.com>
2025-10-27 08:48:31 +0100

    Cuadrado de num y num2

--- calc.py
+++ calc.py
@@ -6,3 +6,7 @@ num = 4
 num2 = 5
 multiplicacion = num * num2
 print(multiplicacion)
+
+cuadrado = num ** 2
+cuadrado2 = num2 ** 2
+print(f"El cuadrado del primer numero es: {cuadrado}, el cuadrado del segundo numero es: {cuadrado2}")
